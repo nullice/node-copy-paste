6579a9f86f3a48b141dae3f2ff78fbe35bb01286
nullice <ui@nullice.com>
2017-07-15 18:30:16 +0800

    MacOS: pbcopy/pbpaste use UTF-8 need env variables "LANG"
    MacOS: pbcopy/pbpaste use UTF-8 need env variables "LANG", else use loacle encoding

--- platform/darwin.js
+++ platform/darwin.js
@@ -1,5 +1,5 @@
-exports.copy = { command: "pbcopy", args: [] };
-exports.paste = { command: "pbpaste", args: [] };
+exports.copy = { command: "pbcopy", args: {env:{LANG:"en_US.UTF-8"}} };
+exports.paste = { command: "pbpaste", args: {env:{LANG:"en_US.UTF-8"}} };
 exports.paste.full_command = exports.paste.command;
 exports.encode = function(str) { return new Buffer(str, "utf8"); };
 exports.decode = function(chunks) {
